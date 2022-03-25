c42d27cef65c9445978c0ace4cd869ea310edf35
alejomj19971 <amuneraja@cesde.net>
2022-03-24 19:34:10 -0500

    si

---
+++ controlador2.js
@@ -0,0 +1,65 @@
+
+function getMensaje(){
+    return console.log("Hola grupo")
+    
+
+}
+let variable=getMensaje();
+
+console.log(variable);
+
+function inicioSesion(){
+    let inicioSesion=true;
+    return inicioSesion;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+
+}
+
+const nombre=()=>{
+    HTMLFormControlsCollection.LOG("hola grupo")
+}
+
+
+
+
+
+
+let numero1=4;
+let numero2=3;
+
+function sumar(numero1,numero2){
+   let suma=numero1+numero2;
+    return suma;
+    
+}
+
+function restar(numero1,numero2){
+   let resta=numero1-numero2;
+    return resta;
+    
+}
+
+function multiplicar(numero1,numero2){
+ let producto=numero1*numero2;
+    return producto;
+    
+}
+
+function dividir(numero1,numero2){
+    let divide=numero1/numero2;
+    
+    if(numero2==0){
+       return divide="No se puede dividir por 0"
+    }
+    return divide;
+    
+}
+
+const resultadoSuma=sumar(numero1,numero2);
+const resultadoResta=restar(numero1,numero2);
+const resultadoProducto=multiplicar(numero1,numero2);
+const resultadoDivide=dividir(numero1,numero2);
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoProducto);
+console.log(resultadoDivide);
